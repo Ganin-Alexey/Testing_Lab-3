a4fd808d085687a43637f6100bd03d25e2a58d46
Ganin <anonim.minona.80@mail.ru>
2023-03-13 00:28:27 +0700

    V-1.0.1 [ADD]: Created method fill_file in Cleaner

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,19 @@
+import os
+
 
 class Cleaner:
-    pass
+    @staticmethod
+    def fill_file(path: str, char: str = 'r', offset: int = 3) -> bool:
+        if os.path.isfile(path):
+            with open(file=path, mode='w+', encoding='utf8') as file:
+                for line in file.readlines():
+                    count_of_chars = len(line)
+                    print(char, count_of_chars, offset)
+                    print(char * count_of_chars * offset)
+                    file.write(str(char * count_of_chars * offset))
+            return True
+        return False
 
 
 if __name__ == '__main__':
-    pass
+    print('g' * 5 * 5)
--- tests.py
+++ tests.py
@@ -5,7 +5,10 @@ from cleaner import Cleaner
 class CleanerTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.cleaner = Cleaner()
+        cls.filepath = 'dir_with_data/file_with_data.txt'
+        cls.dir_path = 'dir_with_data/'
+        cls.char = 'g'
 
     # def tearDownClass(self):
     #     pass
@@ -14,8 +17,15 @@ class CleanerTestCase(unittest.TestCase):
         pass
 
     def test_create_class(self):
-        cleaner = Cleaner()
-        self.assertIsInstance(cleaner, Cleaner)
+        self.assertIsInstance(self.cleaner, Cleaner)
+
+    def test_fill_file(self):
+        result = self.cleaner.fill_file(path=self.filepath, char=self.char)
+        self.assertTrue(result)
+        with open(file=self.filepath, mode='r') as file:
+            for line in file.readlines():
+                for char in line:
+                    self.assertEqual(self.char, char)
 
 
 if __name__ == '__main__':
